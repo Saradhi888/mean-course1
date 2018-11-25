c938d4e9e7b38aa734d9b3d50e8c341b927510f5
Vijay Saradhi <vijay.saradhi888@gmail.com>
2018-11-25 04:13:14 -0500

    added one component createPost

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { PostCreateComponent} from './posts/create-posts/post-create.component'
 @NgModule({
   declarations: [
     AppComponent
+    PostCreateComponent
   ],
   imports: [
     BrowserModule,
